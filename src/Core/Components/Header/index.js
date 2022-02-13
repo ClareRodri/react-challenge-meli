054bc834c049c2c6258ff0b4aba09aacd7740d9a
Clarena Rodriguez <clarena.rodriguez@mvm.com.co>
2022-02-13 00:26:41 -0500

    feat: Add first version search bar styles

---
+++ src/Core/Components/Header/index.js
@@ -0,0 +1,10 @@
+
+import './index.scss';
+import React from 'react';
+import SearchBar from '../../../Shared/Components/SearchBar';
+
+export default function Header() {
+    return <>
+        <SearchBar></SearchBar>
+    </>
+}
--- src/Shared/Components/SearchBar/index.js
+++ src/Shared/Components/SearchBar/index.js
@@ -1,5 +1,17 @@
+import './index.scss';
 import React from 'react';
 
 export default function SearchBar() {
-    return <></>
+    return <>
+        <a className='search-logo-meli'>Mercado Libre Colombia - Donde comprar y vender de todo</a>
+        <form className='search-form'>
+            <input className='search-input'
+                placeholder='Busca productos y más...'
+                type='text'>
+            </input>
+            <button className='search-button'>
+                🔎
+            </button>
+        </form>
+    </>
 }

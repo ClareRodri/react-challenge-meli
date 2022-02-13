5dfcff9d9545e99a337e9f087172932791016500
Clarena Rodriguez <clarena.rodriguez@mvm.com.co>
2022-02-13 15:56:04 -0500

    feat: Add first version Breadcrumb

--- src/Shared/Components/BreadCrumb/index.js
+++ src/Shared/Components/BreadCrumb/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import './index.scss';
+
+const CATEGORIAS = [
+    { title: 'Electrónica Audio y Video', weigth: 10 },
+    { title: 'iPod', weigth: 22 },
+    { title: 'Reproductores', weigth: 1 },
+    { title: 'iPod touch', weigth: 4 },
+    { title: '32GB', weigth: 5 },
+]
 
 export default function BreadCrumb() {
-    return <></>
+    return (
+        <div className='my-3'>
+            {CATEGORIAS.map(({ title }, key) =>
+            {
+                const url = `/search/${title}`;
+                return (CATEGORIAS.length - 1 === key)
+                    ? (<span className='breadcrumb-title' key={key}>
+                            <strong>{title}</strong>
+                        </span>)
+                    : (<span>
+                            <Link className='breadcrumb-title' key={key} to={url}> {title}</Link>
+                            <span className='breadcrumb-title mx-2'> {'>'} </span>
+                        </span>)
+               }
+            )}
+        </div>
+    );
 }

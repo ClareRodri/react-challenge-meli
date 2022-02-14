c95b34a75ab67de59e522139552e2c24b50f5cde
Clarena Rodriguez <clarena.rodriguez@mvm.com.co>
2022-02-14 15:48:19 -0500

    feat: Add validation for empty search

--- src/Components/Product/ListProducts/index.js
+++ src/Components/Product/ListProducts/index.js
@@ -3,14 +3,16 @@ import React from 'react';
 import ProductItem from '../ProducItem';
 
 export default function ListProducts({ products }) {
+
     return <div className='product-list'>
-        {
-            products.items.map((item) => (
+        {   products.items.length > 0
+            ? products.items.map((item) => (
                     <>
                     <ProductItem item={item}></ProductItem>
                     </>
                 )
             )
+            : <i className='empty'>No se encontraron resultados</i>
         }
     </div>
 }
--- src/Services/GetProduct.js
+++ src/Services/GetProduct.js
@@ -1,9 +1,13 @@
 export default function getProduct({ sKeyword = "" } = {}) {
-    const apiURL = `/api/items?q=${sKeyword}`;
+    const offset = 0;
+    const limit = 4;
+    const apiURL = `/api/items?q=${sKeyword}&offset=${offset}&limit=${limit}`;
     return fetch(apiURL)
         .then(rest => rest.json())
         .then(resp => {
-            const { author, items, categories } = resp;
+            let { author, items, categories } = resp;
+            items = items || [];
+            categories = categories || [];
             return { author, items, categories };
         });
 }
